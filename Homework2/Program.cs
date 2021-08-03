423427340795636651758803751e74e554350e6b
Alexey Vlasenko <alexinhex@gmail.com>
2021-08-03 19:55:52 +0300

    Добавил цвета в консоль на каждый день недели.

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -19,13 +19,11 @@ namespace Homework2
         static void Main(string[] args)
         {
             string dayToday;
-            string dayOfWeekNow;
 
 
             DateTime dt = DateTime.Now;
             DayOfWeek day = dt.DayOfWeek;
-            dayOfWeekNow = day.ToString();
-
+            
 
             Console.WriteLine("Какой сегодня день?");
             dayToday = Console.ReadLine();
@@ -33,42 +31,49 @@ namespace Homework2
             switch (dayToday)
             {
                 case "Mon":
+                    Console.BackgroundColor = ConsoleColor.Red;
                     Console.WriteLine("Сегодня " + (Days)1 + " 1-й день недели, до выходного еще 4 дня");
                     if (day == DayOfWeek.Monday)
                         Console.WriteLine("Сегодня ПОНЕДЕЛЬНИК!!!");
                     break;
 
                 case "Tue":
+                    Console.BackgroundColor = ConsoleColor.DarkYellow;
                     Console.WriteLine("Сегодня " + (Days)2 + " 2-й день недели, до выходного еще 3 дня");
                     if (day == DayOfWeek.Tuesday)
                         Console.WriteLine("Сегодня ВТОРНИК!!!");
                     break;
 
                 case "Wed":
+                    Console.BackgroundColor = ConsoleColor.Yellow;
                     Console.WriteLine("Сегодня " + (Days)3 + " 3-й день недели, до выходного еще 2 дня");
                     if (day == DayOfWeek.Wednesday)
                         Console.WriteLine("Сегодня СРЕДА!!!");
                     break;
 
                 case "Thu":
+                    Console.BackgroundColor = ConsoleColor.Green;
                     Console.WriteLine("Сегодня " + (Days)4 + " 4-й день недели, до выходного еще 1 день!");
                     if (day == DayOfWeek.Thursday)
                         Console.WriteLine("Сегодня Четверг!!!");
                     break;
 
                 case "Fri":
+                    Console.BackgroundColor = ConsoleColor.Blue;
                     Console.WriteLine("Сегодня " + (Days)5 + " 5-й день недели, завтра выходной!!!");
                     if (day == DayOfWeek.Friday)
                         Console.WriteLine("Сегодня ПЯТНИЦА!!!");
                     break;
 
                 case "Sat":
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
                     Console.WriteLine("Сегодня " + (Days)6 + " 6-й день недели, сегодня можно лечь попозже");
                     if (day == DayOfWeek.Saturday)
                         Console.WriteLine("Сегодня СУББОТА!!!");
                     break;
 
                 case "Sun":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
                     Console.WriteLine("Сегодня " + (Days)7 + " 7-й день недели, завтра на работку!");
                     if (day == DayOfWeek.Sunday)
                         Console.WriteLine("Сегодня ВОСКРЕСЕНЬЕ!!!");
